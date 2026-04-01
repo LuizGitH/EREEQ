a982880beb21783538fe66436d2aff4e1d79cc4c
Luiz <luis2016felip@gmail.com>
2026-04-01 00:46:37 -0300

    feat: finish EventImages component

--- src/components/EventImages/index.jsx
+++ src/components/EventImages/index.jsx
@@ -3,12 +3,16 @@ export function EventImages() {
         <section className="w-full bg-[#FFF6E9] py-12 md:py-20">
             <div className="max-w-6xl mx-auto px-4 md:px-6 font-sans text-justify">
 
-                <h2 className="text-2xl md:text-4xl font-bold text-[#351205] mb-6">
+                <h2 className="text-2xl md:text-4xl font-bold text-[#351205] mb-8">
                     Imagens do Evento
                 </h2>
 
 
-                <div>
+                <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6
+                    [&>img]:w-full [&>img]:h-[220px] [&>img]:object-cover
+                    [&>img]:rounded-xl [&>img]:border-4 [&>img]:border-black [&>img]:shadow-md
+                    [&>img]:transition [&>img]:duration-300 [&>img]:hover:scale-105">
+
                     <img src="./EventIMG/image_01.jpg" alt="" />
                     <img src="./EventIMG/image_02.jpg" alt="" />
                     <img src="./EventIMG/image_03.jpg" alt="" />
